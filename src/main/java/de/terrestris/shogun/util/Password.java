9e4c19afe9dd4a1c49433be60f71db4beed9a00f
Marc Jansen <jansen@terrestris.de>
2013-02-18 15:31:47 +0100

    Smaller refactoring of password util.

--- src/main/java/de/terrestris/shogun/util/Password.java
+++ src/main/java/de/terrestris/shogun/util/Password.java
@@ -1,21 +1,19 @@
-/**
- * 
- */
 package de.terrestris.shogun.util;
 
 import java.util.Random;
 
 /**
  * Helper class for password related issues
+ *
+ * @author terrestris GmbH & Co. KG
  */
 public class Password {
-	
+
 	/**
-	 * the charset of which the PW consists of
+	 * A set of characters to build passwords
 	 */
-	private static final String CHARSET = "!0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	
-	
+	private static final String PW_CHARSET = "_!0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 	/**
 	 * Helper method to create a random password
 	 * 
@@ -23,16 +21,13 @@ public class Password {
 	 * @return the new password
 	 */
 	public static String getRandomPassword(int length) {
-		
-		String charset = CHARSET;
 
 		Random rand = new Random(System.currentTimeMillis());
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < length; i++) {
-			int pos = rand.nextInt(charset.length());
-			sb.append(charset.charAt(pos));
+			int pos = rand.nextInt(PW_CHARSET.length());
+			sb.append(PW_CHARSET.charAt(pos));
 		}
 		return sb.toString();
 	}
-
 }
