58dff4d76f42c2fe2d8a6b49d126ee384e6321ea
Nils Buehner <buehner@terrestris.de>
2013-08-08 16:50:55 +0200

    Repaired failed build. Using apache-commons-lang3 now.

--- src/main/java/de/terrestris/shogun/security/ShogunAuthProcessingFilter.java
+++ src/main/java/de/terrestris/shogun/security/ShogunAuthProcessingFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonGenerator;
 import org.springframework.security.core.Authentication;
