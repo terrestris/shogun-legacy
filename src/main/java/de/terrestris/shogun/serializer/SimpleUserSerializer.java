63cb32840a8d4aeebcfaa0ae15fad6f1f4a34019
Johannes Weskamm <weskamm@terrestris.de>
2013-08-15 03:24:10 -0700

    User / Group Administration
      - Added new JSON-userserializer to display owner field as integer on MapLayers
      - Removed jsonignore from maplayer model on owner field to display
        the owner (user id)

---
+++ src/main/java/de/terrestris/shogun/serializer/SimpleUserSerializer.java
@@ -0,0 +1,29 @@
+package de.terrestris.shogun.serializer;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.JsonSerializer;
+import org.codehaus.jackson.map.SerializerProvider;
+
+import de.terrestris.shogun.model.User;
+
+/**
+ * A serializer that takes the user object of a model
+ * and returns an id of the corresponding user.
+ * 
+ * @author terrestris GmbH & Co. KG
+ * 
+ */
+
+public class SimpleUserSerializer extends JsonSerializer<User> {
+
+	@Override
+	public void serialize(User value, JsonGenerator jgen,
+			SerializerProvider provider) throws IOException,
+			JsonProcessingException {
+		jgen.writeNumber(value.getId());
+	}
+
+}
