09f7feb4c517a54ae5efec8b5dd4f8f2be5b48bd
Marc Jansen <jansen@terrestris.de>
2014-02-13 00:23:24 -0800

    Avoid "null null" usernames when serializing Users.
    Also adds two comments regarding the addition of that field.
    These should be adressed in the future.

--- src/main/java/de/terrestris/shogun/serializer/SimpleUserSerializer.java
+++ src/main/java/de/terrestris/shogun/serializer/SimpleUserSerializer.java
@@ -24,8 +24,28 @@ public class SimpleUserSerializer extends JsonSerializer<User> {
 			SerializerProvider provider) throws IOException,
 			JsonProcessingException {
 		jgen.writeNumber(value.getId());
-		jgen.writeStringField("ownerName", value.getUser_firstname() + " " +
-				value.getUser_lastname());
+
+		// Also serialize the name of the user into a field "ownerName"
+		// TODO We shouldn't assume that we always are serializing
+		//      an "owner" (hence "ownerName" might be not very good decision)
+		// TODO In case we have multiple User.class entities that are serialized
+		//      this might lead to an illegal (?) JSON, as duplicate keys might
+		//      be produced. I do not know how all parsers deal with this.
+		//      "The names within an object SHOULD be unique."
+		//      http://www.ietf.org/rfc/rfc4627.txt
+		String ownerName = null;
+		if (value.getUser_firstname() != null) {
+			ownerName = value.getUser_firstname();
+		}
+		if (value.getUser_lastname() != null) {
+			if (ownerName == null) {
+				ownerName = "";
+			} else {
+				ownerName += " ";
+			}
+			ownerName += value.getUser_lastname();
+		}
+		jgen.writeStringField("ownerName", ownerName);
 	}
 
 }
