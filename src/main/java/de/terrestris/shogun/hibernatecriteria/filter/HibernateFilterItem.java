8a97316fb7fa5d2c6cad49542c62d03f28bded9b
Marc Jansen <jansen@terrestris.de>
2014-04-15 15:46:38 +0200

    Add support for dotted fieldnames in FilterItem.

--- src/main/java/de/terrestris/shogun/hibernatecriteria/filter/HibernateFilterItem.java
+++ src/main/java/de/terrestris/shogun/hibernatecriteria/filter/HibernateFilterItem.java
@@ -47,83 +47,134 @@ public class HibernateFilterItem extends FilterItem {
 		Criterion criterion = null;
 
 		int operandCount = this.getOperands().size();
-		String fieldName = this.getFieldName();
 
+		String fieldName = this.getFieldName();
+		String originalFieldName = fieldName;
+		
+		// when we were called with a fieldname that contains a dot,
+		// we have to change the mainClass and the fieldanem
+		if (fieldName != null && fieldName.contains(".")) {
+			String[] parts = fieldName.split("\\.");
+			Class clz = this.getClassFromFieldName(parts[0], mainClass);
+			mainClass = clz;
+			fieldName = parts[1];
+		}
+		
+		
 		Object finalOperand = null;
 		Object finalOperand2 = null;
 		
 		if (this.getOperator() != Operator.Statement) {
 			// Cast the operands to the correct data type of the DB field
-			finalOperand = castOperandToFinalDataType(mainClass, fieldName, (String) this.getOperands().get(0).getOperand());
+			finalOperand = castOperandToFinalDataType(mainClass, fieldName, this.getOperands().get(0).getOperand().toString());
+			
 			finalOperand2 = null;
 
 			if (operandCount == 2) {
-				finalOperand2 = castOperandToFinalDataType(mainClass, fieldName, (String) this.getOperands().get(1).getOperand());
+				finalOperand2 = castOperandToFinalDataType(mainClass, fieldName, this.getOperands().get(1).getOperand().toString());
 			}
 		} else {
 			finalOperand = (String) this.getOperands().get(0).getOperand();
 		}
-
+		
 		switch (this.getOperator()) {
 		case Any:
 			break;
 		case Between:
 			if (operandCount == 2) {
-				criterion = Restrictions.between(fieldName, finalOperand, finalOperand2);
+				criterion = Restrictions.between(originalFieldName, finalOperand, finalOperand2);
 			}
 			break;
 		case Equals:
-			criterion = Restrictions.eq(fieldName, finalOperand);
+			criterion = Restrictions.eq(originalFieldName, finalOperand);
 			break;
 		case Greater:
-			criterion = Restrictions.gt(fieldName, finalOperand);
+			criterion = Restrictions.gt(originalFieldName, finalOperand);
 			break;
 		case GreaterEq:
-			criterion = Restrictions.ge(fieldName, finalOperand);
+			criterion = Restrictions.ge(originalFieldName, finalOperand);
 			break;
 		case In:
 			Object[] values = new Object[operandCount];
 			for (int o = 0; o < operandCount; o++) {
 				values[o] = finalOperand;
 			}
-			criterion = Restrictions.in(fieldName, values);
+			criterion = Restrictions.in(originalFieldName, values);
 			break;
 		case IsNull:
-			criterion = Restrictions.isNull(fieldName);
+			criterion = Restrictions.isNull(originalFieldName);
 			break;
 		case Like:
-			criterion = Restrictions.like(fieldName, finalOperand);
+			criterion = Restrictions.like(originalFieldName, finalOperand);
 			break;
 		case ILike:
 			//TODO remove the ternary operator
 			// introduce exclude fields in client
-			criterion = (finalOperand != null) ? Restrictions.ilike(fieldName, finalOperand) : null;
+			criterion = (finalOperand != null) ? Restrictions.ilike(originalFieldName, finalOperand) : null;
 			break;
 		case NotEquals:
-			criterion = Restrictions.ne(fieldName, finalOperand);
+			criterion = Restrictions.ne(originalFieldName, finalOperand);
 			break;
 		case NotLike:
-			criterion = Restrictions.not(Restrictions.like(fieldName, finalOperand));
+			criterion = Restrictions.not(Restrictions.like(originalFieldName, finalOperand));
 			break;
 		case NotNull:
-			criterion = Restrictions.isNotNull(fieldName);
+			criterion = Restrictions.isNotNull(originalFieldName);
 			break;
 		case Smaller:
-			criterion = Restrictions.lt(fieldName, finalOperand);
+			criterion = Restrictions.lt(originalFieldName, finalOperand);
 			break;
 		case SmallerEq:
-			criterion = Restrictions.le(fieldName, finalOperand);
+			criterion = Restrictions.le(originalFieldName, finalOperand);
 			break;
 		case Statement:
 			criterion = Restrictions.sqlRestriction((String)finalOperand);
 			break;
 		case DWithin:
-			criterion = SpatialRestrictions.within(fieldName, (Geometry)finalOperand);
+			criterion = SpatialRestrictions.within(originalFieldName, (Geometry)finalOperand);
 			break;
 		}
 		return criterion;
 	}
 	
+	/**
+	 * 
+	 * 
+	 * 
+	 * @param fieldName
+	 * @param mainClass
+	 * @return
+	 */
+	private Class getClassFromFieldName(String fieldName, Class mainClass) {
+		Class c = null;
+		try {
+			List<Field> allFields = getAllFields(new ArrayList<Field>(), mainClass);
+			Field f = null;
+			for (int i = 0; i < allFields.size(); i++) {
+				if (allFields.get(i).getName().equals(fieldName)) {
+					f = allFields.get(i);
+					break;
+				}
+			}
+			
+			Type t = f.getGenericType();
+			
+			if (t instanceof ParameterizedType) {
+				ParameterizedType pt = (ParameterizedType) t;
+				for (Type ttt : pt.getActualTypeArguments()) {
+					c = (Class) ttt;
+					break;
+				}
+			}
+			
+		} catch (SecurityException e) {
+			LOGGER.warn("Failed to determine the ParameterizedType of Set " +
+					fieldName + " in class " + mainClass.getSimpleName());
+		}
+		
+		return c;
+	}
+
 	/**
 	 * Cast the operand to the correct data type of the model (DB)
 	 * 
@@ -133,9 +184,7 @@ public class HibernateFilterItem extends FilterItem {
 	 * @return
 	 */
 	private Object castOperandToFinalDataType(Class mainClass, String fieldName, String operand) {
-
 		Object finalOperand = null;
-
 		List<Field> fieldList = new ArrayList<Field>();
 		fieldList = this.getAllFields(fieldList, mainClass);
 		
@@ -143,11 +192,9 @@ public class HibernateFilterItem extends FilterItem {
 			
 			Field field = fieldList.get(j);
 			
-			if (field.getName().equals(this.getFieldName())) {
-				
+			if (field.getName().equals(fieldName)) {
 				// Class Integer
 				if (field.getType().equals(java.lang.Integer.class)) {
-					
 					finalOperand = new Integer(operand);
 				}
 				// data type int
@@ -214,14 +261,26 @@ public class HibernateFilterItem extends FilterItem {
 				else {
 					finalOperand = operand;
 				}
+				
+				// found a datatype
+				break;
 			}
 
+		}
+		
+		if (finalOperand == null) {
+			LOGGER.warn("Failed to determine the final data-type of field " +
+				fieldName + " in class " + mainClass.getSimpleName() +
+				" for operand " + operand + ". Falling back to datatype java.lang.String.");
+			finalOperand = new String(operand);
 		}
 
 		return finalOperand;
 	}
 	
 	/**
+	 * TODO we have a very similar method in {@link DatabaseDao}.
+	 * 
 	 * 
 	 * @param fields
 	 * @param type
