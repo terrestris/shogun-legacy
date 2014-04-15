f51203e0b493edc8bc6a3934c4eb787f6735e656
Marc Jansen <jansen@terrestris.de>
2014-04-15 14:15:02 +0200

    More docs and fewer warnings.

--- src/main/java/de/terrestris/shogun/serializer/LeanBaseModelSerializer.java
+++ src/main/java/de/terrestris/shogun/serializer/LeanBaseModelSerializer.java
@@ -12,18 +12,19 @@ import org.codehaus.jackson.map.SerializerProvider;
 import de.terrestris.shogun.model.BaseModelInterface;
 
 /**
- * A serializer that can handle java.util.Date-instances.
+ * A serializer that takes sets of instances of {@link BaseModelInterface}
+ * and returns a an array of the ids of the instances as serialisation value.
  * 
  * @author terrestris GmbH & Co. KG
  * 
  */
+@SuppressWarnings("rawtypes")
 public class LeanBaseModelSerializer extends JsonSerializer<Set> {
 
 	@Override
 	public void serialize(Set value, JsonGenerator jgen,
 			SerializerProvider provider) throws IOException,
 			JsonProcessingException {
-		
 		jgen.writeStartArray();
 		for (Iterator iterator = value.iterator(); iterator.hasNext();) {
 			BaseModelInterface object = (BaseModelInterface) iterator.next();
